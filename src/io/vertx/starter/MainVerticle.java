6b52e00bbdf10b4a2ed4cd6f2a6bc75c776c075f
Marcin Czeczko <marcin.czeczko@cognifide.com>
2018-03-29 12:20:58 +0200

    Added logging to sample class

--- src/io/vertx/starter/MainVerticle.java
+++ src/io/vertx/starter/MainVerticle.java
@@ -1,11 +1,15 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
+ private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
 
   @Override
   public void start() {
+    LOGGER.info("Starting HTTP server");
     vertx.createHttpServer()
         .requestHandler(req -> req.response().end("Hello Vert.x !"))
         .listen(8080);
